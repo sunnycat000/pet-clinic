2444dd026c495fc70c280c3282ceac70a1e4e0da
sunnycat000 <yanlily0111@gmail.com>
2021-03-18 13:17:33 -0700

    Initial Project Add

---
+++ src/test/java/yan/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package yan.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
