70d8b24de3fd82d48e66b8ec199f433cbb3e2da4
sunnycat000 <yanlily0111@gmail.com>
2021-03-20 15:59:32 -0700

    Implement Interface for Services. Closes #5.

---
+++ pet-clinic-web/src/test/java/yan/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package yan.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = {PetClinicApplication.class})
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
