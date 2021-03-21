a5c3d7b93574283497198d279450a6d928b9e5a7
sunnycat000 <yanlily0111@gmail.com>
2021-03-20 17:13:56 -0700

    Adding base entity. Closes #10.

--- pet-clinic-data/src/main/java/yan/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/yan/petclinic/model/Pet.java
@@ -2,7 +2,7 @@ package yan.petclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
     private PetType petType;
     private Owner owner;
     private LocalDate birthDate;
