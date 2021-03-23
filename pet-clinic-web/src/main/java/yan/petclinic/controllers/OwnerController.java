10207e2db026598543cd65780530c655c549af86
sunnycat000 <yanlily0111@gmail.com>
2021-03-22 23:14:57 -0700

    Add Owners List and Update Controller. Closes #16.

--- pet-clinic-web/src/main/java/yan/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/yan/petclinic/controllers/OwnerController.java
@@ -1,13 +1,24 @@
 package yan.petclinic.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import yan.petclinic.services.OwnerService;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/","/index","/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
