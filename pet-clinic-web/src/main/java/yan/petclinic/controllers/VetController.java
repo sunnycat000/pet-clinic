2468a71890b1b850165207651ae02e1ed8e9e622
sunnycat000 <yanlily0111@gmail.com>
2021-03-22 23:27:26 -0700

    Add Vets List and Update Controller. Closes #15.

--- pet-clinic-web/src/main/java/yan/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/yan/petclinic/controllers/VetController.java
@@ -1,12 +1,23 @@
 package yan.petclinic.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import yan.petclinic.services.VetService;
 
 @Controller
 public class VetController {
+    private final VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","vets/index","vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
