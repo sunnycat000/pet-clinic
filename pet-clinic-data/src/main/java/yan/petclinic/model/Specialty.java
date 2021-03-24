246c8e64e05ad37aa18c025b2b0a80d26928a50c
sunnycat000 <yanlily0111@gmail.com>
2021-03-24 15:51:47 -0700

    Create Visit Entity, Update Owner with Pet Relationship. Closes #26

---
+++ pet-clinic-data/src/main/java/yan/petclinic/model/Specialty.java
@@ -0,0 +1,13 @@
+package yan.petclinic.model;
+
+public class Specialty extends BaseEntity{
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
