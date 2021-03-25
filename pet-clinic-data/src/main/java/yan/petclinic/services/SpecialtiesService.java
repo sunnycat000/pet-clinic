1a20f3de78eab56f871d561d1d0aaecc93fbc483
sunnycat000 <yanlily0111@gmail.com>
2021-03-24 21:28:04 -0700

    Add Specialty Map Implementation. Closes #31

---
+++ pet-clinic-data/src/main/java/yan/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package yan.petclinic.services;
+
+import yan.petclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
--- pet-clinic-data/src/main/java/yan/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/yan/petclinic/services/VetService.java
@@ -2,7 +2,5 @@ package yan.petclinic.services;
 
 import yan.petclinic.model.Vet;
 
-import java.util.Set;
-
 public interface VetService extends CrudService<Vet, Long>{
 }
