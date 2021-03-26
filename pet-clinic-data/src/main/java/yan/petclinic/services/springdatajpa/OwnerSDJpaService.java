0e580db4971dbd0ab635cb43e1599d35ebe9f56d
sunnycat000 <yanlily0111@gmail.com>
2021-03-25 21:02:43 -0700

    Create Spring Data JPA for Vets. Closes #41

--- pet-clinic-data/src/main/java/yan/petclinic/services/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/yan/petclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -4,27 +4,18 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import yan.petclinic.model.Owner;
 import yan.petclinic.repositories.OwnerRepository;
-import yan.petclinic.repositories.PetRepository;
-import yan.petclinic.repositories.PetTypeRepository;
 import yan.petclinic.services.OwnerService;
-import yan.petclinic.services.PetTypeService;
 
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
 
 @Service
 @Profile("springdatajpa")
 public class OwnerSDJpaService implements OwnerService {
     private final OwnerRepository ownerRepository;
-    private final PetRepository petRepository;
-    private final PetTypeRepository petTypeRepository;
 
-    public OwnerSDJpaService(OwnerRepository ownerRepository, PetRepository petRepository,
-                             PetTypeRepository petTypeRepository) {
+    public OwnerSDJpaService(OwnerRepository ownerRepository) {
         this.ownerRepository = ownerRepository;
-        this.petRepository = petRepository;
-        this.petTypeRepository = petTypeRepository;
     }
 
     @Override
